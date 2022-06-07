a5968be98b42106a02e3798169f07e95103f5f25
krich9871 <64726640+krich9871@users.noreply.github.com>
2022-06-07 13:45:49 +0700

    project 5 more css

--- project-6/src/App.js
+++ project-6/src/App.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
-
+import { FaChevronCircleRight } from "react-icons/fa";
+const url = 'https://course-api.com/react-tabs-project'
 function App() {
+  const [Loading, SetLoading] = useState(true) ;
   return (
     <>
-    
+     <h2></h2>
     </>
   );
 }
